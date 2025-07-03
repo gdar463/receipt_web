53c41a253a999035eea7716f503d9227f797a3c1
gdar463 <gdar463@gmail.com>
2025-07-03 18:18:30 +0200

    added sort imports

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,5 +1,16 @@
 import { type Config } from "prettier";
 
-const config: Config = {};
+const config: Config = {
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  importOrder: [
+    "^(bun|path|os|sys|stream)",
+    "<THIRD_PARTY_MODULES>",
+    "^@/.*",
+    "^../",
+    "^./",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
 
 export default config;
