3b43313443a35b1a0e07ab4e87252d9c3a52d0e5
gdar463 <gdar463@gmail.com>
2025-07-04 14:04:14 +0200

    added react-cookie package

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
+import { CookiesProvider } from "react-cookie";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
@@ -18,9 +19,11 @@ declare module "@tanstack/react-router" {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <CookiesProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </CookiesProvider>
   </StrictMode>,
 );
