7ac30a0ca9ebfb0a0022421119dddc4d02230d9f
gdar463 <gdar463@gmail.com>
2025-07-04 15:26:16 +0200

    removed test dashboard

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,31 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { useCookies } from "react-cookie";
-
-import { Skeleton } from "@/components/ui/skeleton";
-import { apiMe } from "@/lib/api/auth";
 
 export const Route = createFileRoute("/dashboard/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const [cookies] = useCookies(["session"]);
-
-  const query = useQuery({
-    queryKey: ["apiMe", cookies.session],
-    queryFn: () => apiMe(cookies.session),
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
-  });
-
-  return (
-    <div>
-      {query.isSuccess ? (
-        <div>{query.data.displayName}</div>
-      ) : (
-        <Skeleton className="h-[20px] w-[100px] rounded-full" />
-      )}
-    </div>
-  );
+  return <div></div>;
 }
