a461dcb1aff91e620cb3c12e75b516e280615841
gdar463 <gdar463@gmail.com>
2025-07-04 15:06:20 +0200

    added alert description to login alert

--- src/lib/alerts/login.tsx
+++ src/lib/alerts/login.tsx
@@ -1,24 +1,27 @@
 import { AlertCircleIcon } from "lucide-react";
 
-import { Alert, AlertTitle } from "@/components/ui/alert";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 import type { ApiError } from "../api/errors";
 
 export function LoginAlert({ error }: { error: ApiError }) {
-  let message;
+  let title, message;
   switch (error.body.code) {
     case "InvalidCreds":
-      message = "Invalid username or password.";
+      title = "Invalid username or password.";
+      message = "Try again.";
       break;
     default:
-      message = "Generic Error.";
+      title = "Generic Error.";
+      message = "Check the browser console for more information.";
       break;
   }
 
   return (
     <Alert variant="destructive" className="mb-2 [&>svg]:size-5">
       <AlertCircleIcon />
-      <AlertTitle className="text-base">{message}</AlertTitle>
+      <AlertTitle className="text-base">{title}</AlertTitle>
+      <AlertDescription>{message}</AlertDescription>
     </Alert>
   );
 }
