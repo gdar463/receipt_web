785ff37c9476bc8864140c28510cfdb7728b39b1
gdar463 <gdar463@gmail.com>
2025-07-14 10:36:46 +0200

    configured and formatted eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,30 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
-import pluginQuery from "@tanstack/eslint-plugin-query"
-import eslintConfigPrettier from 'eslint-config-prettier'
+import js from "@eslint/js";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+import eslintConfigPrettier from "eslint-config-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    rules: {
+      "react-refresh/only-export-components": "off",
+    },
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      pluginQuery.configs['flat/recommended'],
-      reactHooks.configs['recommended-latest'],
+      pluginQuery.configs["flat/recommended"],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
-      eslintConfigPrettier
+      eslintConfigPrettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-])
+]);
