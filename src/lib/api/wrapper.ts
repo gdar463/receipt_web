7cef05d25fee06cad8a465b9ab19a21cc6bad19b
gdar463 <gdar463@gmail.com>
2025-07-16 13:02:57 +0200

    moved wrapper hooks to start of file

--- src/lib/api/wrapper.ts
+++ src/lib/api/wrapper.ts
@@ -1,40 +1,34 @@
-import type { Options } from "ky";
+import type { HTTPError, Options } from "ky";
 import ky from "ky";
 
 import { ApiError } from "./errors";
 
 const API_URL = import.meta.env.API_URL || "http://localhost:3000/api";
 
+const hooks = {
+  hooks: {
+    beforeError: [
+      async (error: HTTPError<unknown>) => {
+        const { response } = error;
+        if (response && response.body) {
+          throw new ApiError(await response.json(), response.status);
+        }
+        return error;
+      },
+    ],
+  },
+};
+
 export async function get(route: string, options?: Options) {
   return ky.get(API_URL + route, {
+    ...hooks,
     ...options,
-    hooks: {
-      beforeError: [
-        async (error) => {
-          const { response } = error;
-          if (response && response.body) {
-            throw new ApiError(await response.json(), response.status);
-          }
-          return error;
-        },
-      ],
-    },
   });
 }
 
 export async function post(route: string, options?: Options) {
   return ky.post(API_URL + route, {
     ...options,
-    hooks: {
-      beforeError: [
-        async (error) => {
-          const { response } = error;
-          if (response && response.body) {
-            throw new ApiError(await response.json(), response.status);
-          }
-          return error;
-        },
-      ],
-    },
+    ...hooks,
   });
 }
