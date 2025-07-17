51634546caea5615838727acdb32849805ae7646
gdar463 <gdar463@gmail.com>
2025-07-17 17:55:07 +0200

    formatted files

--- src/routes/_authed/dashboard/list.tsx
+++ src/routes/_authed/dashboard/list.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_authed/dashboard/list')({
+export const Route = createFileRoute("/_authed/dashboard/list")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_authed/dashboard/list"!</div>
+  return <div>Hello "/_authed/dashboard/list"!</div>;
 }
