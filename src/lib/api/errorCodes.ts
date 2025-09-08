1024833193dc14c86ab10c2534fb13971a52067d
gdar463 <gdar463@gmail.com>
2025-09-08 15:23:24 +0200

    added google invalid grant error code

--- src/lib/api/errorCodes.ts
+++ src/lib/api/errorCodes.ts
@@ -8,6 +8,7 @@ export type ErrorCodes =
   | "CurrencyNotFound"
   | "ValidationFailed"
   | "NoGoogleLinked"
+  | "GoogleInvalidGrant"
   | "GoogleFailed"
   | "ReceiptNotFound"
   | "NameAlreadyExists"
